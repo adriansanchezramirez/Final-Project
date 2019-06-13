199100d2b7bf6d678b7b6471911cf43f36da1414
adriansanchezramirez <adrisanchez39@gmail.com>
2019-06-13 21:53:52 +0200

    Add API

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+$db = [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'db' => 'afinal' 
+];
+?>
---
+++ api/post.php
@@ -0,0 +1,79 @@
+<?php
+include "config.php";
+include "utils.php";
+$dbConn =  connect($db);
+/*
+  listar todos los posts o solo uno
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * FROM posts where id=:id");
+      $sql->bindValue(':id', $_GET['id']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+    }
+    else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM posts");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+  }
+}
+// Crear un nuevo post
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $input = $_POST;
+    $sql = "INSERT INTO posts
+          (title, status, content, user_id)
+          VALUES
+          (:title, :status, :content, :user_id)";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+    $postId = $dbConn->lastInsertId();
+    if($postId)
+    {
+      $input['id'] = $postId;
+      header("HTTP/1.1 200 OK");
+      echo json_encode($input);
+      exit();
+   }
+}
+//Borrar
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+  $id = $_GET['id'];
+  $statement = $dbConn->prepare("DELETE FROM posts where id=:id");
+  $statement->bindValue(':id', $id);
+  $statement->execute();
+  header("HTTP/1.1 200 OK");
+  exit();
+}
+//Actualizar
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $postId = $input['id'];
+    $fields = getParams($input);
+    $sql = "
+          UPDATE posts
+          SET $fields
+          WHERE id='$postId'
+           ";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
+?>
